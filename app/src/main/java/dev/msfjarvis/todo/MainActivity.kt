4ddd01b77cef4ea93d611f7a7ac6bd98ce353154
Harsh Shandilya <me@msfjarvis.dev>
2020-08-22 20:12:35 +0530

    Remove unused import
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/todo/MainActivity.kt
+++ app/src/main/java/dev/msfjarvis/todo/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign

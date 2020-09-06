3ed1cdb261829486663ad9e335f0fcf02ec9865a
Harsh Shandilya <me@msfjarvis.dev>
2020-09-06 23:05:09 +0530

    TodoTheme: use default colors but with system theme awareness
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/todo/ui/TodoTheme.kt
+++ app/src/main/java/dev/msfjarvis/todo/ui/TodoTheme.kt
@@ -1,11 +1,15 @@
 package dev.msfjarvis.todo.ui
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 @Composable
 fun TodoTheme(children: @Composable () -> Unit) {
   MaterialTheme(
+    colors = if (isSystemInDarkTheme()) darkColors() else lightColors(),
     content = children,
   )
 }

64cc897f1a3589a95846b48b397ec21dd8e71200
Harsh Shandilya <me@msfjarvis.dev>
2020-09-07 12:09:24 +0530

    Add missing import
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/todo/ui/TodoTheme.kt
+++ app/src/main/java/dev/msfjarvis/todo/ui/TodoTheme.kt
@@ -2,6 +2,7 @@ package dev.msfjarvis.todo.ui
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable

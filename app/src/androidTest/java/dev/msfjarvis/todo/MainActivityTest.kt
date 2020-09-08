e0e28b5e35eff3c0a2c2183e66333b099f0d4030
Harsh Shandilya <me@msfjarvis.dev>
2020-09-08 05:54:24 +0530

    Add test to check item add dialog is shown
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/androidTest/java/dev/msfjarvis/todo/MainActivityTest.kt
+++ app/src/androidTest/java/dev/msfjarvis/todo/MainActivityTest.kt
@@ -2,7 +2,8 @@ package dev.msfjarvis.todo
 
 import androidx.ui.test.assertIsDisplayed
 import androidx.ui.test.createComposeRule
-import androidx.ui.test.onNodeWithText
+import androidx.ui.test.onNodeWithTag
+import androidx.ui.test.performClick
 import dev.msfjarvis.todo.data.model.TodoItem
 import dev.msfjarvis.todo.ui.TodoTheme
 import org.junit.Rule
@@ -14,10 +15,10 @@ class MainActivityTest {
   val composeTestRule = createComposeRule()
 
   @Test
-  fun test_item_addition() {
+  fun item_add_dialog_shows_on_fab_click() {
     composeTestRule.setContent {
       TodoTheme {
-        val items = arrayListOf(TodoItem("Item 1"))
+        val items = arrayListOf<TodoItem>()
         TodoApp(
           items,
           items::add,
@@ -25,7 +26,7 @@ class MainActivityTest {
         )
       }
     }
-    //onNode(hasTestTag("fab")).performClick()
-    onNodeWithText("Item 1").assertIsDisplayed()
+    onNodeWithTag("fab").performClick()
+    onNodeWithTag("item_dialog").assertIsDisplayed()
   }
 }

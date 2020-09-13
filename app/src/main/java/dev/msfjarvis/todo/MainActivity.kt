94258b8d97ecb6c6ef2b978d67b3b858873797de
Harsh Shandilya <me@msfjarvis.dev>
2020-09-13 23:24:52 +0530

    MainActivity: drop hardcoded top appbar title
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/todo/MainActivity.kt
+++ app/src/main/java/dev/msfjarvis/todo/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
@@ -73,7 +74,7 @@ fun TodoApp(
   }
 
   Scaffold(
-    topBar = { TopAppBar({ Text(text = "I can Compose?") }) },
+    topBar = { TopAppBar({ Text(text = stringResource(R.string.app_name)) }) },
     floatingActionButton = {
       FloatingActionButton(
         onClick = { showingDialog.value = true },

b68eda30511d66a94800416c8040381d0543d3e8
Harsh Shandilya <me@msfjarvis.dev>
2020-09-25 02:04:33 +0530

    app: tweak padding for tags and add border
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/lobsters/ui/LobstersItem.kt
+++ app/src/main/java/dev/msfjarvis/lobsters/ui/LobstersItem.kt
@@ -1,7 +1,9 @@
 package dev.msfjarvis.lobsters.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -46,8 +48,9 @@ fun LazyItemScope.LobstersItem(
             Text(
               text = tag,
               modifier = Modifier
+                .border(BorderStroke(1.dp, Color.Gray))
                 .background(Color(0xFFFFFCD7), RoundedCornerShape(4.dp))
-                .padding(4.dp),
+                .padding(vertical = 2.dp, horizontal = 4.dp),
               color = Color.DarkGray,
             )
           }

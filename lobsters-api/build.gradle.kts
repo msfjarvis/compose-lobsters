msfjarvis/compose-lobsters

commit 84f45f523aba55256b4aed933b48e33d585eb76f

Replace Moshi with kotlinx.serialization
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

lobsters-api/build.gradle.kts
@@ -1,18 +1,14 @@
 plugins {
   id("com.android.library")
   kotlin("android")
-  kotlin("kapt")
   kotlin("plugin.serialization") version "1.4.10"
   `lobsters-plugin`
 }
 
 dependencies {
   implementation(project(":model"))
-  implementation(Dependencies.ThirdParty.Retrofit.lib)
-  implementation(Dependencies.ThirdParty.Retrofit.moshi)
   implementation(Dependencies.Kotlin.Serialization.json)
   implementation(Dependencies.ThirdParty.retrofitSerialization)
-  kaptTest(Dependencies.ThirdParty.Moshi.codegen)
   testImplementation(Dependencies.Testing.junit)
   testImplementation(Dependencies.Kotlin.Coroutines.core)
   testImplementation(Dependencies.Testing.mockWebServer)

app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   id("com.android.application")
   kotlin("android")
   kotlin("kapt")
+  kotlin("plugin.serialization") version "1.4.10"
   id("dagger.hilt.android.plugin")
   `lobsters-plugin`
   `core-library-desugaring`
@@ -90,9 +91,9 @@ dependencies {
   implementation(Dependencies.ThirdParty.Roomigrant.runtime)
   implementation(Dependencies.AndroidX.material)
   implementation(Dependencies.AndroidX.Hilt.dagger)
-  implementation(Dependencies.ThirdParty.Moshi.lib)
   implementation(Dependencies.ThirdParty.accompanist)
   implementation(Dependencies.Kotlin.Coroutines.android)
+  implementation(Dependencies.Kotlin.Serialization.json)
   implementation(Dependencies.ThirdParty.customtabs)
   androidTestImplementation(Dependencies.Testing.daggerHilt)
   testImplementation(Dependencies.Testing.junit)

model/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
   id("com.android.library")
   kotlin("android")
-  kotlin("kapt")
   kotlin("plugin.serialization") version "1.4.10"
   `lobsters-plugin`
 }
 
 dependencies {
-  kapt(Dependencies.ThirdParty.Moshi.codegen)
-  implementation(Dependencies.ThirdParty.Moshi.lib)
   implementation(Dependencies.Kotlin.Serialization.json)
 }

app/src/main/java/dev/msfjarvis/lobsters/data/source/LobstersApiTypeConverters.kt
@@ -1,24 +1,24 @@
 package dev.msfjarvis.lobsters.data.source
 
 import androidx.room.TypeConverter
-import com.squareup.moshi.Moshi
 import dev.msfjarvis.lobsters.model.Submitter
-import dev.msfjarvis.lobsters.model.SubmitterJsonAdapter
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 object LobstersApiTypeConverters {
-  private val moshi = Moshi.Builder().build()
   private const val SEPARATOR = ","
 
   @TypeConverter
   @JvmStatic
   fun toSubmitterUser(value: String?): Submitter? {
-    return value?.let { SubmitterJsonAdapter(moshi).fromJson(value) }
+    return value?.let { Json.decodeFromString(value) }
   }
 
   @TypeConverter
   @JvmStatic
   fun fromSubmitterUser(value: Submitter?): String? {
-    return value?.let { SubmitterJsonAdapter(moshi).toJson(value) }
+    return value?.let { Json.encodeToString(value) }
   }
 
   @TypeConverter

app/src/main/java/dev/msfjarvis/lobsters/ui/posts/LobstersItem.kt
@@ -136,7 +136,7 @@ fun PreviewLobstersItem() {
       "sevan",
       null,
       null,
-      null,
+      emptyList(),
     ),
     listOf("openbsd"),
   )

buildSrc/src/main/java/Dependencies.kt
@@ -79,18 +79,10 @@ object Dependencies {
     const val customtabs = "saschpe.android:customtabs:3.0.2"
     const val retrofitSerialization = "com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0"
 
-    object Moshi {
-
-      private const val version = "1.11.0"
-      const val codegen = "com.squareup.moshi:moshi-kotlin-codegen:$version"
-      const val lib = "com.squareup.moshi:moshi:$version"
-    }
-
     object Retrofit {
 
       private const val version = "2.9.0"
       const val lib = "com.squareup.retrofit2:retrofit:$version"
-      const val moshi = "com.squareup.retrofit2:converter-moshi:$version"
     }
 
     object Roomigrant {

lobsters-api/src/main/java/dev/msfjarvis/lobsters/api/ApiClient.kt
@@ -1,13 +1,15 @@
 package dev.msfjarvis.lobsters.api
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 object ApiClient {
   inline fun <reified T> getClient(baseUrl: String): T {
     return Retrofit.Builder()
       .baseUrl(baseUrl)
-      .addConverterFactory(MoshiConverterFactory.create())
+      .addConverterFactory(Json.asConverterFactory(MediaType.get("application/json")))
       .build()
       .create(T::class.java)
   }

model/src/main/java/dev/msfjarvis/lobsters/model/KeybaseSignature.kt
@@ -1,12 +1,12 @@
 package dev.msfjarvis.lobsters.model
 
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-@JsonClass(generateAdapter = true)
+@Serializable
 class KeybaseSignature(
-  @Json(name = "kb_username")
+  @SerialName("kb_username")
   val kbUsername: String,
-  @Json(name = "sig_hash")
+  @SerialName("sig_hash")
   val sigHash: String
 )

commit dc12917b66acf0ccf01c1a3eb522dfe7690c270f

lobsters-api: add back accidentally removed Retrofit dependency
Fixes: 84f45f523aba ("Replace Moshi with kotlinx.serialization")
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

lobsters-api/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 dependencies {
   implementation(project(":model"))
   implementation(Dependencies.Kotlin.Serialization.json)
+  implementation(Dependencies.ThirdParty.Retrofit.lib)
   implementation(Dependencies.ThirdParty.retrofitSerialization)
   testImplementation(Dependencies.Testing.junit)
   testImplementation(Dependencies.Kotlin.Coroutines.core)
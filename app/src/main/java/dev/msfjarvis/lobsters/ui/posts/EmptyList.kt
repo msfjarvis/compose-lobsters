ff4dfcac610bedda45b4e301213288fc814664bc
Harsh Shandilya <me@msfjarvis.dev>
2020-11-12 01:54:20 +0530

    treewide: update Compose and other deps
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/lobsters/ui/posts/EmptyList.kt
+++ app/src/main/java/dev/msfjarvis/lobsters/ui/posts/EmptyList.kt
@@ -1,10 +1,10 @@
 package dev.msfjarvis.lobsters.ui.posts
 
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

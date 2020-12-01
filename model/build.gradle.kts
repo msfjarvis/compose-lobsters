c29edfd4029bffa7bcd2bcdffdbbdc432b79b7a0
Harsh Shandilya <me@msfjarvis.dev>
2020-12-01 22:37:19 +0530

    model: import API related classes
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
   id("com.android.library")
   kotlin("android")
+  kotlin("kapt")
   kotlin("plugin.serialization") version "1.4.21"
   `lobsters-plugin`
 }
 
 dependencies {
+  kapt(Dependencies.AndroidX.Hilt.daggerCompiler)
+  kapt(Dependencies.AndroidX.Hilt.daggerHiltCompiler)
+  api(Dependencies.Kotlin.Ktor.clientCore)
+  implementation(Dependencies.AndroidX.Hilt.dagger)
   implementation(Dependencies.Kotlin.Serialization.json)
+  implementation(Dependencies.Kotlin.Ktor.clientJson)
+  implementation(Dependencies.Kotlin.Ktor.clientOkHttp)
+  implementation(Dependencies.Kotlin.Ktor.clientSerialization)
+  testImplementation(Dependencies.Testing.junit)
+  testImplementation(Dependencies.Kotlin.Ktor.clientTest)
 }

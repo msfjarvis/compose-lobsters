a4f05c744e8161f7b4ec70d0c3826e99ee7c2106
Harsh Shandilya <me@msfjarvis.dev>
2020-12-24 04:45:47 +0530

    build: target Java 11
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- buildSrc/src/main/java/BaseProjectConfig.kt
+++ buildSrc/src/main/java/BaseProjectConfig.kt
@@ -102,8 +102,8 @@ internal fun TestedExtension.configureCommonAndroidOptions() {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
   }
 
   testOptions.animationsDisabled = true

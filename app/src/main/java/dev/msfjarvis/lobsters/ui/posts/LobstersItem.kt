33b238c64625f7e83db058cb0dd62e369db48167
Harsh Shandilya <me@msfjarvis.dev>
2021-01-28 13:45:13 +0530

    LobstersItem: extract hard-coded submitter text
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/lobsters/ui/posts/LobstersItem.kt
+++ app/src/main/java/dev/msfjarvis/lobsters/ui/posts/LobstersItem.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -109,7 +110,7 @@ fun LobstersItem(
           },
       )
       Text(
-        text = "submitted by ${post.submitterUser.username}",
+        text = stringResource(id = R.string.submitted_by, post.submitterUser.username),
         modifier = Modifier
           .padding(4.dp)
           .constrainAs(submitter) {

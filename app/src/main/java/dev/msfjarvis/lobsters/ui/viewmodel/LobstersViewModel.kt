9ce33bba750e6d0b56957c805371b47b140fc317
Aditya Wasan <adityawasan55@gmail.com>
2021-01-31 23:02:16 +0530

    src: add isCacheReady to let users know cache status
    Signed-off-by: Aditya Wasan <adityawasan55@gmail.com>

--- app/src/main/java/dev/msfjarvis/lobsters/ui/viewmodel/LobstersViewModel.kt
+++ app/src/main/java/dev/msfjarvis/lobsters/ui/viewmodel/LobstersViewModel.kt
@@ -11,6 +11,7 @@ import dev.msfjarvis.lobsters.data.remote.LobstersPagingSource
 import dev.msfjarvis.lobsters.data.repo.LobstersRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,11 +20,19 @@ class LobstersViewModel @Inject constructor(
   private val lobstersRepository: LobstersRepository,
   private val pagingSource: LobstersPagingSource,
 ) : ViewModel() {
-  private val _savedPosts = MutableStateFlow(lobstersRepository.getAllPostsFromCache())
+  private val _savedPosts = MutableStateFlow<List<LobstersPost>>(emptyList())
   val savedPosts = _savedPosts.asStateFlow()
   val posts = Pager(PagingConfig(25)) {
     pagingSource
   }.flow.cachedIn(viewModelScope)
+
+  init {
+    viewModelScope.launch {
+      lobstersRepository.isCacheReady.onEach {
+        _savedPosts.value = lobstersRepository.getAllPostsFromCache()
+      }
+    }
+  }
 
   fun toggleSave(post: LobstersPost) {
     viewModelScope.launch {

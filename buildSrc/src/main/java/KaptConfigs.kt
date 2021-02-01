2a4c58a6807cf1bdfb0ff2a58c562a178e4b0a44
Harsh Shandilya <me@msfjarvis.dev>
2021-02-01 18:29:22 +0530

    buildSrc: conditionally configure Dagger and disable kapt in tests
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- buildSrc/src/main/java/KaptConfigs.kt
+++ buildSrc/src/main/java/KaptConfigs.kt
@@ -8,10 +8,24 @@ import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 internal fun Project.configureKapt() {
   extensions.configure<KaptExtension> {
     javacOptions {
-      option("-Adagger.fastInit=enabled")
-      option("-Adagger.experimentalDaggerErrorMessages=enabled")
-      option("-Xmaxerrs", 500)
-      option("-Adagger.moduleBindingValidation=ERROR")
+      if (hasDaggerCompilerDependency) {
+        option("-Adagger.fastInit=enabled")
+        option("-Adagger.strictMultibindingValidation=enabled")
+        option("-Adagger.experimentalDaggerErrorMessages=enabled")
+        option("-Xmaxerrs", 500)
+        option("-Adagger.moduleBindingValidation=ERROR")
+      }
     }
+    // disable kapt tasks for unit tests
+    tasks.matching {
+      it.name.startsWith("kapt") && it.name.endsWith("UnitTestKotlin")
+    }.configureEach { enabled = false }
   }
 }
+
+private val Project.hasDaggerCompilerDependency: Boolean
+  get() = configurations.any {
+    it.dependencies.any { dependency ->
+      dependency.name == "dagger-compiler"
+    }
+  }

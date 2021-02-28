7e7609ca1fa1487f75d4c44c9f7ead1ee16ce4ed
Harsh Shandilya <me@msfjarvis.dev>
2021-02-28 19:48:55 +0530

    build: force Moshi to latest version
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
   `lobsters-plugin`
 }
+
+subprojects {
+  configurations.configureEach {
+    resolutionStrategy {
+      // Retrofit depends on a very old version of Moshi that causes moshi-ksp to fail
+      force(Dependencies.ThirdParty.Moshi.lib)
+    }
+  }
+}

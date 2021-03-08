08c511f85460f83a6b642a78aa04b682de03cfcf
Harsh Shandilya <me@msfjarvis.dev>
2021-03-08 16:22:39 +0530

    posts: use a shimmering animation for loading posts
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/lobsters/ui/posts/SavedPosts.kt
+++ app/src/main/java/dev/msfjarvis/lobsters/ui/posts/SavedPosts.kt
@@ -1,13 +1,24 @@
 package dev.msfjarvis.lobsters.ui.posts
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import dev.msfjarvis.lobsters.R
 import dev.msfjarvis.lobsters.data.local.SavedPost
 import dev.msfjarvis.lobsters.ui.urllauncher.LocalUrlLauncher
+import dev.msfjarvis.lobsters.util.IconResource
 import dev.msfjarvis.lobsters.util.asZonedDateTime
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -21,7 +32,19 @@ fun SavedPosts(
   val urlLauncher = LocalUrlLauncher.current
 
   if (posts.isEmpty()) {
-    EmptyList(saved = true)
+    Column(
+      modifier = Modifier.fillMaxSize(),
+      verticalArrangement = Arrangement.Center,
+      horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+      IconResource(
+        R.drawable.ic_favorite_border_24px,
+        tint = Color(0xFFD97373),
+        modifier = Modifier.padding(16.dp),
+        contentDescription = stringResource(R.string.add_to_saved_posts),
+      )
+      Text(stringResource(R.string.no_saved_posts))
+    }
   } else {
     LazyColumn(
       state = listState,

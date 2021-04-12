654eb39877524d9b5649140c21e13c43dd16dbd0
Harsh Shandilya <me@msfjarvis.dev>
2021-04-13 02:17:22 +0530

    app: de-duplicate paging sources
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/lobsters/data/remote/LobstersPagingSource.kt
+++ app/src/main/java/dev/msfjarvis/lobsters/data/remote/LobstersPagingSource.kt
@@ -2,20 +2,16 @@ package dev.msfjarvis.lobsters.data.remote
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import dev.msfjarvis.lobsters.data.repo.LobstersRepository
 import dev.msfjarvis.lobsters.model.LobstersPost
 
-class NewestPostsPagingSource(
-  private val lobstersRepository: LobstersRepository,
+class LobstersPagingSource(
+  private val getMorePosts: suspend (Int) -> List<LobstersPost>,
 ) : PagingSource<Int, LobstersPost>() {
 
   override suspend fun load(params: LoadParams<Int>): LoadResult<Int, LobstersPost> {
     return try {
       val page = params.key ?: 1
-      // Update cache before fetching a list.
-      // This is done to make sure that we can update the isSaved status of incoming posts.
-      lobstersRepository.updateCache()
-      val posts = lobstersRepository.fetchNewestPosts(page)
+      val posts = getMorePosts(page)
 
       LoadResult.Page(
         data = posts,

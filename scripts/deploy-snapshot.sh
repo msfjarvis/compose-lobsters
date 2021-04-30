85a7d0df8418d906f2f934ad928fa2dda06c7f6a
Harsh Shandilya <me@msfjarvis.dev>
2021-04-30 10:10:24 +0530

    scripts/deploy-snapshot: adapt for removal of variant output API
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -3,11 +3,12 @@
 set -euo pipefail
 
 export SSHDIR="$HOME/.ssh"
+export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
+export VERSION_NAME="$(cat app/version.properties | grep versionName | cut -d '=' -f 2)"
 mkdir -p "$SSHDIR"
 echo "$ACTIONS_DEPLOY_KEY" > "$SSHDIR/key"
 chmod 600 "$SSHDIR/key"
-export SERVER_DEPLOY_STRING="$SSH_USERNAME@$SERVER_ADDRESS:$SERVER_DESTINATION"
 mkdir -p "$GITHUB_WORKSPACE/Claw"
-cp -v ./app/build/outputs/apk/release/*.apk "$GITHUB_WORKSPACE/Claw/"
+cp -v ./app/build/outputs/apk/release/app-release.apk "$GITHUB_WORKSPACE/Claw/Claw-${VERSION_NAME}.apk"
 cd "$GITHUB_WORKSPACE/Claw"
 rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING"

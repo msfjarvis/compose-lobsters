f60d1c72461085090d1bab53b879cb69396ddbc4
Harsh Shandilya <me@msfjarvis.dev>
2021-06-04 16:27:20 +0530

    common: clip avatars to circle shape
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- common/src/commonMain/kotlin/dev/msfjarvis/claw/common/posts/LobstersItem.kt
+++ common/src/commonMain/kotlin/dev/msfjarvis/claw/common/posts/LobstersItem.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.IconButton
 import androidx.compose.material.IconToggleButton
@@ -22,6 +23,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowRow
@@ -131,7 +133,7 @@ fun SubmitterAvatar(
   KamelImage(
     resource = lazyImageResource(avatarUrl),
     contentDescription = contentDescription,
-    modifier = Modifier.requiredSize(24.dp),
+    modifier = Modifier.requiredSize(24.dp).clip(CircleShape),
     crossfade = true,
   )
 }

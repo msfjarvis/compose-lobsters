336a330e94ee99593e4b4d834108c48d7c669c2e
Harsh Shandilya <me@msfjarvis.dev>
2021-06-04 19:06:07 +0530

    desktop: add multiple elements and scrollbar
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,16 +1,43 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import dev.msfjarvis.claw.common.posts.LobstersItem
 import dev.msfjarvis.claw.common.posts.TEST_POST
 import dev.msfjarvis.claw.common.theme.LobstersTheme
 
 fun main() = Window {
-  LobstersTheme(darkTheme = true) {
-    LobstersItem(
-      post = TEST_POST,
-      isSaved = false,
-      {},
-      {},
-      {},
-    )
+  LobstersTheme(darkTheme = false) {
+    Box(
+      modifier = Modifier.fillMaxSize(),
+    ) {
+      val stateVertical = rememberScrollState(0)
+      Box(
+        modifier = Modifier.fillMaxSize().verticalScroll(stateVertical),
+      ) {
+        Column {
+          repeat(10) {
+            LobstersItem(
+              post = TEST_POST,
+              isSaved = false,
+              {},
+              {},
+              {},
+            )
+          }
+        }
+      }
+      VerticalScrollbar(
+        modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
+        adapter = rememberScrollbarAdapter(stateVertical),
+      )
+    }
   }
 }

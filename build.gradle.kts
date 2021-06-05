9478ba3c90a0edde92ee23f89afbe564dbc3896e
Harsh Shandilya <me@msfjarvis.dev>
2021-06-05 14:09:17 +0530

    build: upgrade spotless
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     classpath("com.android.tools.build:gradle:4.1.1")
-    classpath("com.diffplug.spotless:spotless-plugin-gradle:5.12.4")
+    classpath("com.diffplug.spotless:spotless-plugin-gradle:5.12.5")
   }
 }
 

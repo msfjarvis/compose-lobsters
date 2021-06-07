120f28476249ac26ac0df111892c413eb507f36d
Harsh Shandilya <me@msfjarvis.dev>
2021-06-08 00:51:24 +0530

    android: add release signing support
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
   id("org.jetbrains.compose") version "0.4.0"
   id("com.android.application")
@@ -26,6 +28,21 @@ android {
     targetSdkVersion(30)
     versionCode = 1
     versionName = "1.0"
+  }
+  val keystoreConfigFile = rootProject.layout.projectDirectory.file("keystore.properties")
+  if (keystoreConfigFile.asFile.exists()) {
+    val contents = providers.fileContents(keystoreConfigFile).asText.forUseAtConfigurationTime()
+    val keystoreProperties = Properties()
+    keystoreProperties.load(contents.get().byteInputStream())
+    signingConfigs {
+      register("release") {
+        keyAlias = keystoreProperties["keyAlias"] as String
+        keyPassword = keystoreProperties["keyPassword"] as String
+        storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
+        storePassword = keystoreProperties["storePassword"] as String
+      }
+    }
+    buildTypes.all { signingConfig = signingConfigs.getByName("release") }
   }
   buildTypes { getByName("release") { isMinifyEnabled = false } }
   compileOptions {

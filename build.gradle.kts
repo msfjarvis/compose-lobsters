e7febb507c5fe9b96ecd2d2791fdce6e9e79ed84
Harsh Shandilya <me@msfjarvis.dev>
2021-06-11 10:12:53 +0530

    build: bump Hilt to 2.37
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     classpath("com.android.tools.build:gradle:7.1.0-alpha01")
     classpath("com.diffplug.spotless:spotless-plugin-gradle:5.12.5")
-    classpath("com.google.dagger:hilt-android-gradle-plugin:2.36")
+    classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
   }
 }
 

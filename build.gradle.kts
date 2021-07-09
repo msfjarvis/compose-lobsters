7445f7d94ee62c367c60c9bfc5c7bcbafbc694fa
Harsh Shandilya <me@msfjarvis.dev>
2021-07-09 11:19:47 +0530

    build: bump AGP to 7.1.0-alpha03

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
   }
   dependencies {
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-    classpath("com.android.tools.build:gradle:7.1.0-alpha02")
+    classpath("com.android.tools.build:gradle:7.1.0-alpha03")
     classpath("com.diffplug.spotless:spotless-plugin-gradle:5.14.0")
     classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
     classpath("com.android.tools:r8:3.1.13-dev")

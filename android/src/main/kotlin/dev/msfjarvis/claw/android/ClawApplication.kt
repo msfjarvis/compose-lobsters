d850e27dc9b20eda042c4935412e9bda2de1735f
Harsh Shandilya <me@msfjarvis.dev>
2021-08-05 02:13:04 +0530

    android: enable StrictMode

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ClawApplication.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ClawApplication.kt
@@ -1,6 +1,16 @@
 package dev.msfjarvis.claw.android
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp class ClawApplication : Application()
+@HiltAndroidApp
+class ClawApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+    if (BuildConfig.DEBUG) {
+      StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build())
+      StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build())
+    }
+  }
+}

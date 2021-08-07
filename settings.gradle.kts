f5eb62f20f711aa99e004272e1d7604b6256349c
Harsh Shandilya <me@msfjarvis.dev>
2021-08-07 22:28:49 +0530

    gradle: reorder plugin repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 pluginManagement {
   repositories {
-    google()
+    gradlePluginPortal()
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
-    gradlePluginPortal()
+    google()
   }
 }
 

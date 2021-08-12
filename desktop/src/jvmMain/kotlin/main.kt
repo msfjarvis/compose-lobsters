a20a035c47f23271000865c25d42560912de3e18
Harsh Shandilya <me@msfjarvis.dev>
2021-08-12 19:59:25 +0530

    desktop: switch to LobstersCard

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -3,13 +3,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
-import dev.msfjarvis.claw.common.posts.LobstersItem
+import dev.msfjarvis.claw.common.posts.LobstersCard
 import dev.msfjarvis.claw.common.posts.TEST_POST
 import dev.msfjarvis.claw.common.theme.LobstersTheme
 import dev.msfjarvis.claw.common.urllauncher.UrlLauncher
@@ -26,12 +28,13 @@ fun main() = singleWindowApplication {
       ) {
         Column {
           repeat(10) {
-            LobstersItem(
+            LobstersCard(
               post = TEST_POST,
               isSaved = false,
               viewPost = { urlLauncher.launch(TEST_POST.url.ifEmpty { TEST_POST.commentsUrl }) },
               viewComments = { urlLauncher.launch(TEST_POST.commentsUrl) },
               toggleSave = {},
+              modifier = Modifier.padding(bottom = 16.dp, start = 16.dp, end = 16.dp),
             )
           }
         }

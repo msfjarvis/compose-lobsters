558c96a98940cf9ef2fa50f6338f1d4da77ddeeb
Harsh Shandilya <me@msfjarvis.dev>
2021-08-27 00:13:08 +0530

    build: bump Spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     classpath("com.android.tools:r8:3.1.17-dev")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     classpath("com.android.tools.build:gradle:7.1.0-alpha10")
-    classpath("com.diffplug.spotless:spotless-plugin-gradle:5.14.2")
+    classpath("com.diffplug.spotless:spotless-plugin-gradle:5.14.3")
     classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
   }
 }

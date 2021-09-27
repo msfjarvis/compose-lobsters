6037f41d03a480e98127ce942d9351562113461b
Harsh Shandilya <me@msfjarvis.dev>
2021-09-27 19:12:22 +0530

    database: use an on-disk SQLite database

--- database/src/desktopMain/kotlin/dev/msfjarvis/claw/database/local/Database.kt
+++ database/src/desktopMain/kotlin/dev/msfjarvis/claw/database/local/Database.kt
@@ -3,10 +3,14 @@ package dev.msfjarvis.claw.database.local
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import dev.msfjarvis.claw.database.LobstersDatabase
+import java.io.File
 
 actual class DriverFactory {
   actual fun createDriver(): SqlDriver {
-    val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+    val env = System.getenv()
+    val home = env["HOME"]
+    val sqlitePath = "$home${File.pathSeparatorChar}.cache${File.pathSeparatorChar}Claw_database.db"
+    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:$sqlitePath")
     LobstersDatabase.Schema.create(driver)
     return driver
   }

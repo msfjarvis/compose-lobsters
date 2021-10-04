9f8fd43e60a94909115d7242c9d594a82d080034
Harsh Shandilya <me@msfjarvis.dev>
2021-10-04 14:11:41 +0530

    github: revert Debian deployment changes

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -8,7 +8,6 @@ mkdir -p "$SSHDIR"
 echo "$ACTIONS_DEPLOY_KEY" > "$SSHDIR/key"
 chmod 600 "$SSHDIR/key"
 mkdir -p "$GITHUB_WORKSPACE/Claw"
-find . -type f -name '*.apk' -exec cp {} "$GITHUB_WORKSPACE/Claw/Claw.apk" \;
-find . -type f -name '*.deb' -exec cp {} "$GITHUB_WORKSPACE/Claw/" \;
+cp -v ./android/build/outputs/apk/release/android-release.apk "$GITHUB_WORKSPACE/Claw/Claw.apk"
 cd "$GITHUB_WORKSPACE/Claw"
 rsync -ahvcr --omit-dir-times --progress --delete --no-o --no-g -e "ssh -i $SSHDIR/key -o StrictHostKeyChecking=no -p $SSH_PORT" . "$SERVER_DEPLOY_STRING"

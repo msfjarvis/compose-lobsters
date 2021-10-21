4fd13859b3fecb3892f70406fd9195c8e9069eab
Harsh Shandilya <me@msfjarvis.dev>
2021-10-21 10:01:05 +0530

    all: upgrade JetBrains Compose

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
 
 plugins {
-  id("org.jetbrains.compose") version "1.0.0-alpha4-build398"
+  id("org.jetbrains.compose") version "1.0.0-alpha4-build411"
   id("com.android.application")
   kotlin("android")
   kotlin("kapt")
--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
   kotlin("multiplatform")
-  id("org.jetbrains.compose") version "1.0.0-alpha4-build398"
+  id("org.jetbrains.compose") version "1.0.0-alpha4-build411"
 }
 
 group = "dev.msfjarvis.claw"

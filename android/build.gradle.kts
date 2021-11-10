a99ba91f23754531a6ada0ae1e0d6427c54abd3c
Harsh Shandilya <me@msfjarvis.dev>
2021-11-10 11:55:14 +0530

    build: upgrade JB Compose and Aurora

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
 
 plugins {
-  id("org.jetbrains.compose") version "1.0.0-beta6-dev446"
+  id("org.jetbrains.compose") version "1.0.0-beta6-dev450"
   id("com.android.application")
   kotlin("android")
   kotlin("kapt")
--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
   kotlin("multiplatform")
-  id("org.jetbrains.compose") version "1.0.0-beta6-dev446"
+  id("org.jetbrains.compose") version "1.0.0-beta6-dev450"
 }
 
 group = "dev.msfjarvis.claw"

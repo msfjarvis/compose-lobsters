eeddc3a3e648e80d9acb069db9c86a25b21111e9
Harsh Shandilya <me@msfjarvis.dev>
2021-12-09 17:22:22 +0530

    model: mark `User#invitedByUser` as nullable

--- model/src/commonMain/kotlin/dev/msfjarvis/claw/model/User.kt
+++ model/src/commonMain/kotlin/dev/msfjarvis/claw/model/User.kt
@@ -12,7 +12,7 @@ class User(
   @SerialName("is_moderator") val isModerator: Boolean,
   val karma: Int = 0,
   @SerialName("avatar_url") val avatarUrl: String,
-  @SerialName("invited_by_user") val invitedByUser: String,
+  @SerialName("invited_by_user") val invitedByUser: String? = null,
   @SerialName("github_username") val githubUsername: String? = null,
   @SerialName("twitter_username") val twitterUsername: String? = null,
   @SerialName("keybase_signatures") val keybaseSignatures: List<KeybaseSignature> = emptyList(),

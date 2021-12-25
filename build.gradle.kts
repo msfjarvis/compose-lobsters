f363d093f13b360ca050b99b73faf8e376510fa9
Harsh Shandilya <me@msfjarvis.dev>
2021-12-25 18:30:53 +0530

    build: remove wrapper task
    Renovate takes care of this

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,8 +56,3 @@ allprojects {
     }
   }
 }
-
-tasks.withType<Wrapper> {
-  gradleVersion = "7.3.1"
-  distributionSha256Sum = "9afb3ca688fc12c761a0e9e4321e4d24e977a4a8916c8a768b1fe05ddb4d6b66"
-}

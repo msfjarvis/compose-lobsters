4c254cb75e536bd290cebc7e0a88ba891cbc19db
Harsh Shandilya <me@msfjarvis.dev>
2021-12-31 01:27:32 +0530

    android: remove unused modifier

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/ClawAppBar.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/ClawAppBar.kt
@@ -1,13 +1,11 @@
 package dev.msfjarvis.claw.android.ui
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import dev.msfjarvis.claw.android.R
 
 @Composable
@@ -19,7 +17,6 @@ fun ClawAppBar(
       Text(
         text = stringResource(R.string.app_name),
         fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(horizontal = 16.dp),
       )
     },
     modifier = modifier,

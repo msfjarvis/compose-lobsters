1bfa77b319fea4f3f425fa0fc32801003d3e5baa
Harsh Shandilya <me@msfjarvis.dev>
2022-01-07 17:48:40 +0530

    common: remove unused import

--- common/src/commonMain/kotlin/dev/msfjarvis/claw/common/theme/Theme.kt
+++ common/src/commonMain/kotlin/dev/msfjarvis/claw/common/theme/Theme.kt
@@ -7,7 +7,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ProvidedValue
-import androidx.compose.ui.graphics.Color
 
 val LightThemeColors =
   lightColorScheme(

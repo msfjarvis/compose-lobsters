d7bea3fde4bd225e90562e52fb2a6c7e1824f37b
Harsh Shandilya <me@msfjarvis.dev>
2022-02-02 00:11:32 +0530

    model: add an Android target
    Without this Android Studio refuses to import this module

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,11 +1,23 @@
 plugins {
   kotlin("multiplatform")
   kotlin("plugin.serialization")
+  id("com.android.library")
 }
 
 kotlin {
+  android()
   jvm { compilations.all { kotlinOptions.jvmTarget = "11" } }
   sourceSets["commonMain"].apply {
     dependencies { implementation(libs.kotlinx.serialization.core) }
+  }
+}
+
+android {
+  compileSdk = 31
+  sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+  defaultConfig {
+    minSdk = 23
+    targetSdk = 31
+    consumerProguardFiles("consumer-rules.pro")
   }
 }

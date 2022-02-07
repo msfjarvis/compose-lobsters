f2b4bc02f0e533aef5cf24cd2990285c37e34da8
Harsh Shandilya <me@msfjarvis.dev>
2022-02-07 11:57:20 +0530

    common: rename suffixes from px to dp

--- common/src/desktopMain/kotlin/dev/msfjarvis/claw/common/res/drawable.kt
+++ common/src/desktopMain/kotlin/dev/msfjarvis/claw/common/res/drawable.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 
 actual val commentIcon
-  @Composable get() = painterResource("comment_black_24px.svg")
+  @Composable get() = painterResource("comment_black_24dp.svg")
 actual val heartIcon
-  @Composable get() = painterResource("favorite_black_24px.svg")
+  @Composable get() = painterResource("favorite_black_24dp.svg")
 actual val heartBorderIcon
-  @Composable get() = painterResource("favorite_border_black_24px.svg")
+  @Composable get() = painterResource("favorite_border_black_24dp.svg")

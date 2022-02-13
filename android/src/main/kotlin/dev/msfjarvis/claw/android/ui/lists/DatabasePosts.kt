f8a32ccd2cc1f640c2ec92b07aa95f6746c56bab
Harsh Shandilya <me@msfjarvis.dev>
2022-02-14 03:23:56 +0530

    Revert "android: allow swipe to remove from saved posts"
    This reverts commit 80dc5a59f8d7a39d0fb5acb1af89e5b87b632944.

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/lists/DatabasePosts.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/lists/DatabasePosts.kt
@@ -3,16 +3,11 @@ package dev.msfjarvis.claw.android.ui.lists
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.DismissDirection
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.SwipeToDismiss
-import androidx.compose.material.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import dev.msfjarvis.claw.common.posts.PostActions
 import dev.msfjarvis.claw.database.local.SavedPost
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DatabasePosts(
   items: List<SavedPost>,
@@ -26,24 +21,11 @@ fun DatabasePosts(
     modifier = modifier,
   ) {
     items(items) { item ->
-      val dismissState =
-        rememberDismissState(
-          confirmStateChange = {
-            postActions.toggleSave(item)
-            true
-          }
-        )
-      SwipeToDismiss(
-        state = dismissState,
-        directions = setOf(DismissDirection.EndToStart),
-        background = {},
-      ) {
-        ListItem(
-          item = item,
-          isSaved = isSaved,
-          postActions = postActions,
-        )
-      }
+      ListItem(
+        item = item,
+        isSaved = isSaved,
+        postActions = postActions,
+      )
     }
   }
 }

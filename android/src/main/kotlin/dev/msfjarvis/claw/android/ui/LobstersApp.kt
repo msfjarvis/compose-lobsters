8e5bcc78b0cc62aef2853d858b5369423b3aaaa4
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-02-20 13:24:20 +0530

    Hide app bar scroll behaviour
    Hide app bar when scrolling up and show it when scrolling down

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/LobstersApp.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/LobstersApp.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
@@ -56,6 +59,7 @@ fun LobstersApp(
   val coroutineScope = rememberCoroutineScope()
   val postActions = rememberPostActions(urlLauncher, navController, viewModel)
   val currentDestination by currentNavigationDestination(navController)
+  val scrollBehavior = remember { TopAppBarDefaults.enterAlwaysScrollBehavior() }
 
   val networkPosts = viewModel.pagerFlow.collectAsLazyPagingItems()
   val savedPosts by viewModel.savedPosts.collectAsState(emptyList())
@@ -97,10 +101,12 @@ fun LobstersApp(
       }
 
       Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
           ClawAppBar(
-            backgroundColor = systemBarsColor,
             modifier = Modifier.statusBarsPadding(),
+            backgroundColor = systemBarsColor,
+            scrollBehavior = scrollBehavior
           )
         },
         bottomBar = {
--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/decorations/ClawAppBar.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/decorations/ClawAppBar.kt
@@ -3,6 +3,7 @@ package dev.msfjarvis.claw.android.ui.decorations
 import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -13,6 +14,7 @@ import dev.msfjarvis.claw.android.R
 @Composable
 fun ClawAppBar(
   backgroundColor: Color,
+  scrollBehavior: TopAppBarScrollBehavior? = null,
   modifier: Modifier = Modifier,
 ) {
   SmallTopAppBar(
@@ -24,5 +26,6 @@ fun ClawAppBar(
     },
     modifier = modifier,
     colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = backgroundColor),
+    scrollBehavior = scrollBehavior,
   )
 }

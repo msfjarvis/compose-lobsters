b0f99f0b53f5e4c8b9ada2d024a7914d8bcb7aaa
Rahul Krishna <Rahulkrishna585@gmail.com>
2022-02-20 15:58:44 +0530

    Use material3 color for SwipeRefreshIndicator
    Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/lists/HottestPosts.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/lists/HottestPosts.kt
@@ -3,11 +3,14 @@ package dev.msfjarvis.claw.android.ui.lists
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshIndicator
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import dev.msfjarvis.claw.common.posts.PostActions
 import dev.msfjarvis.claw.database.local.SavedPost
@@ -26,6 +29,15 @@ fun HottestPosts(
   SwipeRefresh(
     state = rememberSwipeRefreshState(isRefreshing),
     onRefresh = reloadPosts,
+    indicator = { state, trigger ->
+      val backgroundColor = MaterialTheme.colorScheme.surface
+      SwipeRefreshIndicator(
+        state = state,
+        refreshTriggerDistance = trigger,
+        backgroundColor = backgroundColor,
+        contentColor = contentColorFor(backgroundColor)
+      )
+    }
   ) {
     if (items.itemCount == 0) {
       Box(modifier = Modifier.fillMaxSize())

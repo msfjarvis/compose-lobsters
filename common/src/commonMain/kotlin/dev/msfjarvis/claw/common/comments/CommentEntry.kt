74f2bd1b3bbd04a77cf0fef9857f71de7d19e861
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-02-20 15:52:53 +0530

    Run spotless code format

--- common/src/commonMain/kotlin/dev/msfjarvis/claw/common/comments/CommentEntry.kt
+++ common/src/commonMain/kotlin/dev/msfjarvis/claw/common/comments/CommentEntry.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.halilibo.richtext.markdown.Markdown
 import com.halilibo.richtext.ui.RichTextScope
--- common/src/commonMain/kotlin/dev/msfjarvis/claw/common/comments/Comments.kt
+++ common/src/commonMain/kotlin/dev/msfjarvis/claw/common/comments/Comments.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.msfjarvis.claw.common.NetworkState
 import dev.msfjarvis.claw.common.ui.Divider

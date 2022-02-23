e43b709a1856db3bfd157b9ceb978d381f81d114
Harsh Shandilya <me@msfjarvis.dev>
2022-02-24 01:11:42 +0530

    build: add VCU plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,11 @@ buildscript {
   }
 }
 
-plugins { id("com.diffplug.spotless") version "6.3.0" }
+plugins {
+  id("com.diffplug.spotless") version "6.3.0"
+  id("com.github.ben-manes.versions") version "0.41.0"
+  id("nl.littlerobots.version-catalog-update") version "0.3.0"
+}
 
 group = "dev.msfjarvis.claw"
 

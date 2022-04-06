fe1163be51cc2bd3004d0fc060ea58f24fc1b2ae
Harsh Shandilya <me@msfjarvis.dev>
2022-04-06 12:42:57 +0530

    android: allow existing scheduled work to be replaced
    Without this we end up in situations where changes to constraints
    are not propagated immediately because of pre-scheduled work that
    is retained until it has run.

--- android/src/main/kotlin/dev/msfjarvis/claw/android/MainActivity.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/MainActivity.kt
@@ -48,7 +48,7 @@ class MainActivity : ComponentActivity() {
       WorkManager.getInstance(this@MainActivity)
         .enqueueUniquePeriodicWork(
           "updateSavedPosts",
-          ExistingPeriodicWorkPolicy.KEEP,
+          ExistingPeriodicWorkPolicy.REPLACE,
           postUpdateWorkRequest,
         )
     }

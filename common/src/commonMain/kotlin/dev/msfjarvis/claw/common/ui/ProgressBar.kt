66dc52d244ed10fdd3586ca35f0bc5dee3913631
Harsh Shandilya <me@msfjarvis.dev>
2022-05-05 11:59:42 +0530

    common: extract ProgressBar composable

---
+++ common/src/commonMain/kotlin/dev/msfjarvis/claw/common/ui/ProgressBar.kt
@@ -0,0 +1,16 @@
+package dev.msfjarvis.claw.common.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProgressBar(modifier: Modifier) {
+  Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    CircularProgressIndicator(color = MaterialTheme.colorScheme.secondary)
+  }
+}

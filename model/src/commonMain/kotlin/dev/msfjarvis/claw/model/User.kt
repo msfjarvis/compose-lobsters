5b6e3dc3b94589846b1fbfb5fa01973af141839b
Harsh Shandilya <me@msfjarvis.dev>
2022-05-05 10:55:46 +0530

    model: add more user detail fields
    (cherry picked from commit 083755534867200f06d76934231e8f4cbb1a3f48)

--- model/src/commonMain/kotlin/dev/msfjarvis/claw/model/User.kt
+++ model/src/commonMain/kotlin/dev/msfjarvis/claw/model/User.kt
@@ -6,5 +6,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 class User(
   val username: String,
+  val about: String,
+  val karma: Long,
+  @SerialName("invited_by_user") val invitedBy: String,
   @SerialName("avatar_url") val avatarUrl: String,
+  @SerialName("created_at") val createdAt: String,
 )

642484a9a9411c182092fe3e183c782b95962d46
Rahul Krishna <Rahulkrishna585@gmail.com>
2022-05-07 14:01:19 +0530

    android: use AsyncImage for images
    * Coil recommends using clip instead of CircleCropTransformation for compose.
    * Enable crossfade by default
    * Allow caching images
    * Run spotlessApply
    
    Closes #320
    
    Signed-off-by: Rahul Krishna <Rahulkrishna585@gmail.com>

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ClawApplication.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ClawApplication.kt
@@ -5,11 +5,15 @@ import android.os.StrictMode
 import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
 @HiltAndroidApp
-class ClawApplication : Application(), Configuration.Provider {
+class ClawApplication : Application(), Configuration.Provider, ImageLoaderFactory {
   @Inject lateinit var workerFactory: HiltWorkerFactory
 
   override fun onCreate() {
@@ -24,6 +28,20 @@ class ClawApplication : Application(), Configuration.Provider {
     return Configuration.Builder()
       .setMinimumLoggingLevel(Log.DEBUG)
       .setWorkerFactory(workerFactory)
+      .build()
+  }
+
+  override fun newImageLoader(): ImageLoader {
+    return ImageLoader.Builder(this)
+      .memoryCache { MemoryCache.Builder(this).maxSizePercent(0.25).build() }
+      .diskCache {
+        DiskCache.Builder()
+          .directory(cacheDir.resolve("image_cache"))
+          .maxSizeBytes(25L * 1024 * 1024) // 25 MB
+          .build()
+      }
+      // Show a short crossfade when loading images asynchronously.
+      .crossfade(true)
       .build()
   }
 }
--- common/src/androidMain/kotlin/dev/msfjarvis/claw/common/ui/composables.kt
+++ common/src/androidMain/kotlin/dev/msfjarvis/claw/common/ui/composables.kt
@@ -1,31 +1,20 @@
 package dev.msfjarvis.claw.common.ui
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberAsyncImagePainter
-import coil.request.ImageRequest
-import coil.transform.CircleCropTransformation
+import androidx.compose.ui.draw.clip
+import coil.compose.AsyncImage
 
 @Composable
-@OptIn(ExperimentalCoilApi::class)
 actual fun NetworkImage(
   url: String,
   contentDescription: String,
   modifier: Modifier,
 ) {
-  Image(
-    painter =
-      rememberAsyncImagePainter(
-        ImageRequest.Builder(LocalContext.current)
-          .data(data = url)
-          .transformations(CircleCropTransformation())
-          .crossfade(true)
-          .build()
-      ),
+  AsyncImage(
+    model = url,
     contentDescription = contentDescription,
-    modifier = modifier,
+    modifier = modifier.clip(CircleShape)
   )
 }

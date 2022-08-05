907d044b413e9dfc3d4ee859debd146cf0c41627
Harsh Shandilya <me@msfjarvis.dev>
2022-08-06 02:30:25 +0530

    common: fix Paparazzi tests

--- common/src/test/kotlin/dev/msfjarvis/claw/android/tests/BasePaparazziTest.kt
+++ common/src/test/kotlin/dev/msfjarvis/claw/android/tests/BasePaparazziTest.kt
@@ -1,6 +1,8 @@
 package dev.msfjarvis.claw.android.tests
 
 import app.cash.paparazzi.Paparazzi
+import app.cash.paparazzi.androidHome
+import app.cash.paparazzi.detectEnvironment
 import com.google.testing.junit.testparameterinjector.TestParameter
 import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import org.junit.Rule
@@ -9,6 +11,13 @@ import org.junit.runner.RunWith
 /** Base class for Paparazzi based tests that handles parameterization based on [Theme]. */
 @RunWith(TestParameterInjector::class)
 abstract class BasePaparazziTest {
-  @get:Rule val paparazzi = Paparazzi()
+  @get:Rule
+  val paparazzi =
+    Paparazzi(
+      // https://github.com/cashapp/paparazzi/issues/489
+      environment =
+        detectEnvironment()
+          .copy(platformDir = "${androidHome()}/platforms/android-32", compileSdkVersion = 32)
+    )
   @TestParameter lateinit var theme: Theme
 }

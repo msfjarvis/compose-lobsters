e726bba39f962a29a3970150bf6d556d7d5d83f6
Harsh Shandilya <me@msfjarvis.dev>
2022-09-08 03:07:59 +0530

    refactor(android): update ClawAppBar for Material3 1.0.0-beta02

--- common/src/main/kotlin/dev/msfjarvis/claw/common/ui/decorations/ClawAppBar.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/ui/decorations/ClawAppBar.kt
@@ -1,7 +1,7 @@
 package dev.msfjarvis.claw.common.ui.decorations
 
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SmallTopAppBar
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,7 +15,7 @@ fun ClawAppBar(
   navigationIcon: @Composable () -> Unit = {},
   title: @Composable () -> Unit = {},
 ) {
-  SmallTopAppBar(
+  TopAppBar(
     title = title,
     modifier = modifier,
     colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = backgroundColor),

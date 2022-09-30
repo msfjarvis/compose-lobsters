4bee8f1818aa5fb927c2a1d5604b026acf78df0f
Harsh Shandilya <me@msfjarvis.dev>
2022-09-30 10:46:14 +0530

    feat(common): add preview dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
   implementation(libs.androidx.compose.material3)
   implementation(libs.androidx.compose.runtime)
   implementation(libs.androidx.compose.ui.text)
+  implementation(libs.androidx.compose.ui.tooling)
+  implementation(libs.androidx.compose.ui.tooling.preview)
   implementation(libs.coil.compose)
   implementation(libs.compose.richtext.markdown)
   implementation(libs.compose.richtext.material3)

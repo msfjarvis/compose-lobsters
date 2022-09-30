b5b097b53b508fe2f4bf5d9857a1c83985c83398
Harsh Shandilya <me@msfjarvis.dev>
2022-09-30 11:13:20 +0530

    feat(common): add preview for `ClawAppBar`

--- common/src/main/kotlin/dev/msfjarvis/claw/common/ui/decorations/ClawAppBar.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/ui/decorations/ClawAppBar.kt
@@ -1,11 +1,18 @@
 package dev.msfjarvis.claw.common.ui.decorations
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import dev.msfjarvis.claw.common.theme.LobstersTheme
+import dev.msfjarvis.claw.common.ui.preview.DevicePreviews
+import dev.msfjarvis.claw.common.ui.preview.ThemePreviews
+import dev.msfjarvis.claw.common.ui.surfaceColorAtNavigationBarElevation
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,4 +28,16 @@ fun ClawAppBar(
     colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = backgroundColor),
     navigationIcon = navigationIcon,
   )
+}
+
+@DevicePreviews
+@ThemePreviews
+@Composable
+fun ClawAppBarPreview() {
+  LobstersTheme {
+    ClawAppBar(
+      backgroundColor = MaterialTheme.colorScheme.surfaceColorAtNavigationBarElevation(),
+      title = { Text("Claw", fontWeight = FontWeight.Bold) },
+    )
+  }
 }

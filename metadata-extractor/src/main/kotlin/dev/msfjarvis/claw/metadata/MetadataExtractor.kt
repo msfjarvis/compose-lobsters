52ac8346b3b0fc2e82f699a576bd46f85ecd1e89
Harsh Shandilya <me@msfjarvis.dev>
2022-10-02 16:12:42 +0530

    refactor: resolve Detekt issues found in `metadata-extractor`

--- metadata-extractor/src/main/kotlin/dev/msfjarvis/claw/metadata/MetadataExtractor.kt
+++ metadata-extractor/src/main/kotlin/dev/msfjarvis/claw/metadata/MetadataExtractor.kt
@@ -18,22 +18,25 @@ constructor(
 ) {
 
   suspend fun getExtractedMetadata(url: String): LinkMetadata {
-    val parsedUrl = url.toHttpUrlOrNull() ?: return makeDefault(url)
-    if (!parsedUrl.isHttps) return makeDefault(url)
-    val request = Request.Builder().url(parsedUrl).build()
-    val htmlContent =
-      okHttpClient.newCall(request).execute().use { response ->
-        val body = response.body ?: return makeDefault(url)
-        body.string()
-      }
-    val extractedMetadata = crux.extractFrom(parsedUrl, Jsoup.parse(htmlContent, url))
-    val faviconUrl = extractedMetadata.urls[FAVICON_URL].toString()
-    val readingTime = extractedMetadata[DURATION_MS]
-    return LinkMetadata(
-      url = url,
-      faviconUrl = faviconUrl,
-      readingTime = readingTime,
-    )
+    return run {
+      val parsedUrl = url.toHttpUrlOrNull() ?: return@run null
+      if (!parsedUrl.isHttps) return@run null
+      val request = Request.Builder().url(parsedUrl).build()
+      val htmlContent =
+        okHttpClient.newCall(request).execute().use { response ->
+          val body = response.body ?: return@run null
+          body.string()
+        }
+      val extractedMetadata = crux.extractFrom(parsedUrl, Jsoup.parse(htmlContent, url))
+      val faviconUrl = extractedMetadata.urls[FAVICON_URL].toString()
+      val readingTime = extractedMetadata[DURATION_MS]
+      LinkMetadata(
+        url = url,
+        faviconUrl = faviconUrl,
+        readingTime = readingTime,
+      )
+    }
+      ?: makeDefault(url)
   }
 
   private fun makeDefault(url: String) =

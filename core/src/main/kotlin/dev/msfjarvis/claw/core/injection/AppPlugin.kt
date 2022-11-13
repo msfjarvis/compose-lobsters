0390336d509c10f4d4b9075db5bd1ac9585a9247
Harsh Shandilya <me@msfjarvis.dev>
2022-11-13 20:01:46 +0530

    feat(core): introduce the `AppPlugin` interface

---
+++ core/src/main/kotlin/dev/msfjarvis/claw/core/injection/AppPlugin.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright © 2022 Harsh Shandilya.
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package dev.msfjarvis.claw.core.injection
+
+import android.app.Application
+
+/** Basic plugin-based architecture for doing things at [Application] initialization. */
+interface AppPlugin {
+  fun apply(application: Application)
+}

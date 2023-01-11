fb4beb08ee1d5cde76166da189d6c8647f29d44b
Harsh Shandilya <me@msfjarvis.dev>
2023-01-12 02:40:37 +0530

    fix(common): adopt new Compose APIs

--- common/src/main/kotlin/dev/msfjarvis/claw/common/posts/LobstersCard.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/posts/LobstersCard.kt
@@ -14,6 +14,8 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
@@ -43,7 +45,6 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.flowlayout.FlowRow
 import dev.msfjarvis.claw.common.res.ClawIcons
 import dev.msfjarvis.claw.common.ui.NetworkImage
 import dev.msfjarvis.claw.database.local.SavedPost
@@ -200,14 +201,15 @@ private fun CommentsButton(
 }
 
 @Composable
+@OptIn(ExperimentalLayoutApi::class)
 internal fun TagRow(
   tags: List<String>,
   modifier: Modifier = Modifier,
 ) {
   FlowRow(
     modifier = modifier,
-    mainAxisSpacing = 8.dp,
-    crossAxisSpacing = 8.dp,
+    verticalAlignment = Alignment.CenterVertically,
+    horizontalArrangement = Arrangement.spacedBy(8.dp),
   ) {
     tags.forEach { tag -> TagText(tag) }
   }
--- common/src/main/kotlin/dev/msfjarvis/claw/common/ui/decorations/ClawAppBar.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/ui/decorations/ClawAppBar.kt
@@ -35,7 +35,7 @@ fun ClawAppBar(
   TopAppBar(
     title = title,
     modifier = modifier.shadow(8.dp),
-    colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = backgroundColor),
+    colors = TopAppBarDefaults.topAppBarColors(containerColor = backgroundColor),
     navigationIcon = navigationIcon,
     actions = actions,
   )

8433f38f6e43b0ff48990e32356f32c05e3c614a
Harsh Shandilya <me@msfjarvis.dev>
2023-01-12 00:59:59 +0530

    fix(model): add `Comment#shortId`

--- model/src/main/kotlin/dev/msfjarvis/claw/model/Comment.kt
+++ model/src/main/kotlin/dev/msfjarvis/claw/model/Comment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021-2022 Harsh Shandilya.
+ * Copyright © 2021-2023 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
@@ -15,6 +15,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 class Comment(
+  @SerialName("short_id") val shortId: String,
   val comment: String,
   val url: String,
   val score: Int,

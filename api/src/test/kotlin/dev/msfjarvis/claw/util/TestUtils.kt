9ab8a3e1497916852abeee58fbc1f03d76e7c2e9
Harsh Shandilya <me@msfjarvis.dev>
2023-03-08 01:47:41 +0530

    fix(api): avoid unnecessary build warning

--- api/src/test/kotlin/dev/msfjarvis/claw/util/TestUtils.kt
+++ api/src/test/kotlin/dev/msfjarvis/claw/util/TestUtils.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021-2022 Harsh Shandilya.
+ * Copyright © 2021-2023 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
@@ -11,7 +11,7 @@ import java.io.File
 object TestUtils {
   fun getJson(path: String): String {
     // Load the JSON response
-    val uri = javaClass.classLoader.getResource(path)
+    val uri = javaClass.classLoader!!.getResource(path)
     val file = File(uri.path)
     return String(file.readBytes())
   }

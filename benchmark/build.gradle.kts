293c04b2adb2919aeab1b7d00fe2cc9ebdc10323
Harsh Shandilya <me@msfjarvis.dev>
2023-03-24 14:59:29 +0530

    fix(build): remove `DSL_SCOPE_VIOLATION` suppression

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -1,11 +1,9 @@
 /*
- * Copyright © 2022 Harsh Shandilya.
+ * Copyright © 2022-2023 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
-@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
-
 plugins {
   alias(libs.plugins.android.test)
   id("dev.msfjarvis.claw.android-common")
--- metadata-extractor/build.gradle.kts
+++ metadata-extractor/build.gradle.kts
@@ -4,8 +4,6 @@
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
-@file:Suppress("DSL_SCOPE_VIOLATION", "UnstableApiUsage")
-
 plugins { id("dev.msfjarvis.claw.kotlin-jvm") }
 
 dependencies {

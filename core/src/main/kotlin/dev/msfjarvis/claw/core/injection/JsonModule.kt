b3ff40b12b8eca68915af93fb4fe8913c87e581e
Harsh Shandilya <me@msfjarvis.dev>
2023-07-18 17:11:52 +0530

    refactor(core): `RetrofitModule` -> `JsonModule`

--- core/src/main/kotlin/dev/msfjarvis/claw/core/injection/JsonModule.kt
+++ core/src/main/kotlin/dev/msfjarvis/claw/core/injection/JsonModule.kt
@@ -21,7 +21,7 @@ import retrofit2.Converter
 @Module
 @ContributesTo(ApplicationScope::class)
 @OptIn(ExperimentalSerializationApi::class)
-object RetrofitModule {
+object JsonModule {
 
   @Provides
   @IntoSet

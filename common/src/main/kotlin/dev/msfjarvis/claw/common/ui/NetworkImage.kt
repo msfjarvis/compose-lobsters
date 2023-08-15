67e621160f0067b98d25f6ee3faf304261758d0c
Harsh Shandilya <me@msfjarvis.dev>
2023-08-15 15:37:23 +0530

    fix(common): allow custom tints for NetworkImage placeholders

--- common/src/main/kotlin/dev/msfjarvis/claw/common/ui/NetworkImage.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/ui/NetworkImage.kt
@@ -8,7 +8,9 @@ package dev.msfjarvis.claw.common.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.RenderVectorGroup
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import coil.compose.AsyncImage
 
@@ -18,8 +20,20 @@ internal fun NetworkImage(
   placeholder: ImageVector,
   contentDescription: String,
   modifier: Modifier = Modifier,
+  placeholderTintColor: Color? = null,
 ) {
-  val painter = rememberVectorPainter(placeholder)
+  val painter =
+    rememberVectorPainter(
+      defaultHeight = placeholder.defaultHeight,
+      defaultWidth = placeholder.defaultWidth,
+      viewportWidth = placeholder.viewportWidth,
+      viewportHeight = placeholder.viewportHeight,
+      name = placeholder.name,
+      tintColor = placeholderTintColor ?: Color.Unspecified,
+      tintBlendMode = placeholder.tintBlendMode,
+      autoMirror = placeholder.autoMirror,
+      content = { _, _ -> RenderVectorGroup(group = placeholder.root) },
+    )
   AsyncImage(
     model = url,
     placeholder = painter,

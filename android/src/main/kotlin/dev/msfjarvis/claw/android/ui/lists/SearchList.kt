ba4cd1b0c6c93a3a4ded668bb77e3484122e3fbe
Harsh Shandilya <me@msfjarvis.dev>
2023-11-06 17:29:39 +0530

    fix(android): simplify `SearchList` component

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/lists/SearchList.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/lists/SearchList.kt
@@ -6,24 +6,11 @@
  */
 package dev.msfjarvis.claw.android.ui.lists
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.SearchOff
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.isTraversalGroup
@@ -53,36 +40,19 @@ fun SearchList(
     setSearchQuery(query)
     lazyPagingItems.refresh()
   }
-  var searchActive by remember { mutableStateOf(false) }
   Column(modifier = modifier.semantics { isTraversalGroup = true }.zIndex(1f).fillMaxWidth()) {
     SearchBar(
       value = searchQuery,
       onValueChange = setSearchQuery,
-      onSearch = {
-        triggerSearch(it)
-        searchActive = true
-      },
+      onSearch = { triggerSearch(it) },
       modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp).testTag("search_bar"),
     )
-    if (searchActive) {
-      NetworkPosts(
-        lazyPagingItems = lazyPagingItems,
-        listState = listState,
-        isPostSaved = isPostSaved,
-        isPostRead = { false },
-        postActions = postActions,
-      )
-    } else {
-      Box(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier.align(Alignment.Center)) {
-          Icon(
-            imageVector = Icons.Filled.SearchOff,
-            contentDescription = "No search results",
-            modifier = Modifier.align(Alignment.CenterHorizontally).size(36.dp),
-          )
-          Text(text = "Nothing to see here", style = MaterialTheme.typography.headlineSmall)
-        }
-      }
-    }
+    NetworkPosts(
+      lazyPagingItems = lazyPagingItems,
+      listState = listState,
+      isPostSaved = isPostSaved,
+      isPostRead = { false },
+      postActions = postActions,
+    )
   }
 }

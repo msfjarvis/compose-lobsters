940ff167bfe67bdd8080880e7f575dd955bd3da1
Harsh Shandilya <me@msfjarvis.dev>
2024-01-01 17:10:20 +0530

    refactor(android): extract shared activity logic to BaseActivity

--- android/src/main/kotlin/dev/msfjarvis/claw/android/MainActivity.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/MainActivity.kt
@@ -1,80 +1,36 @@
 /*
- * Copyright © 2021-2023 Harsh Shandilya.
+ * Copyright © 2021-2024 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
 package dev.msfjarvis.claw.android
 
-import android.app.assist.AssistContent
-import android.graphics.Color
-import android.net.Uri
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.runtime.Composable
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.deliveryhero.whetstone.Whetstone
 import com.deliveryhero.whetstone.activity.ContributesActivityInjector
 import dev.msfjarvis.claw.android.ui.screens.LobstersPostsScreen
-import dev.msfjarvis.claw.common.comments.HTMLConverter
-import dev.msfjarvis.claw.common.theme.LobstersTheme
-import dev.msfjarvis.claw.common.urllauncher.UrlLauncher
-import javax.inject.Inject
 
 @ContributesActivityInjector
-class MainActivity : ComponentActivity() {
-
-  @Inject lateinit var urlLauncher: UrlLauncher
-  @Inject lateinit var htmlConverter: HTMLConverter
-  private var webUri: String? = null
+class MainActivity : BaseActivity() {
 
   @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    installSplashScreen()
-    Whetstone.inject(this)
-    enableEdgeToEdge(
-      statusBarStyle =
-        SystemBarStyle.light(
-          Color.TRANSPARENT,
-          Color.TRANSPARENT,
-        ),
-      navigationBarStyle =
-        SystemBarStyle.light(
-          Color.TRANSPARENT,
-          Color.TRANSPARENT,
-        ),
+  @Composable
+  override fun Content() {
+    val windowSizeClass = calculateWindowSizeClass(this)
+    LobstersPostsScreen(
+      urlLauncher = urlLauncher,
+      htmlConverter = htmlConverter,
+      windowSizeClass = windowSizeClass,
+      setWebUri = { url -> webUri = url },
     )
-    setContent {
-      val windowSizeClass = calculateWindowSizeClass(this)
-      LobstersTheme(
-        dynamicColor = true,
-        providedValues = arrayOf(LocalUriHandler provides urlLauncher),
-      ) {
-        LobstersPostsScreen(
-          urlLauncher = urlLauncher,
-          htmlConverter = htmlConverter,
-          windowSizeClass = windowSizeClass,
-          setWebUri = { url -> webUri = url },
-        )
-      }
-    }
   }
 
-  override fun onProvideAssistContent(outContent: AssistContent?) {
-    super.onProvideAssistContent(outContent)
-    if (outContent != null) {
-      if (webUri != null) {
-        outContent.webUri = Uri.parse(webUri)
-      } else {
-        outContent.webUri = null
-      }
-    }
+  override fun preLaunch() {
+    super.preLaunch()
+    installSplashScreen()
   }
 
   companion object {
--- android/src/main/kotlin/dev/msfjarvis/claw/android/SearchActivity.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/SearchActivity.kt
@@ -1,75 +1,24 @@
 /*
- * Copyright © 2021-2023 Harsh Shandilya.
+ * Copyright © 2021-2024 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
  */
 package dev.msfjarvis.claw.android
 
-import android.app.assist.AssistContent
-import android.graphics.Color
-import android.net.Uri
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.platform.LocalUriHandler
-import com.deliveryhero.whetstone.Whetstone
+import androidx.compose.runtime.Composable
 import com.deliveryhero.whetstone.activity.ContributesActivityInjector
 import dev.msfjarvis.claw.android.ui.screens.SearchScreen
-import dev.msfjarvis.claw.android.viewmodel.ClawViewModel
-import dev.msfjarvis.claw.common.comments.HTMLConverter
-import dev.msfjarvis.claw.common.theme.LobstersTheme
-import dev.msfjarvis.claw.common.urllauncher.UrlLauncher
-import javax.inject.Inject
 
 @ContributesActivityInjector
-class SearchActivity : ComponentActivity() {
-
-  @Inject lateinit var urlLauncher: UrlLauncher
-  @Inject lateinit var htmlConverter: HTMLConverter
-  @Inject lateinit var viewModel: ClawViewModel
-  private var webUri: String? = null
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    Whetstone.inject(this)
-    enableEdgeToEdge(
-      statusBarStyle =
-        SystemBarStyle.light(
-          Color.TRANSPARENT,
-          Color.TRANSPARENT,
-        ),
-      navigationBarStyle =
-        SystemBarStyle.light(
-          Color.TRANSPARENT,
-          Color.TRANSPARENT,
-        ),
+class SearchActivity : BaseActivity() {
+  @Composable
+  override fun Content() {
+    SearchScreen(
+      urlLauncher = urlLauncher,
+      htmlConverter = htmlConverter,
+      setWebUri = { webUri = it },
+      viewModel = viewModel,
     )
-    setContent {
-      LobstersTheme(
-        dynamicColor = true,
-        providedValues = arrayOf(LocalUriHandler provides urlLauncher),
-      ) {
-        SearchScreen(
-          urlLauncher = urlLauncher,
-          htmlConverter = htmlConverter,
-          setWebUri = { webUri = it },
-          viewModel = viewModel,
-        )
-      }
-    }
-  }
-
-  override fun onProvideAssistContent(outContent: AssistContent?) {
-    super.onProvideAssistContent(outContent)
-    if (outContent != null) {
-      if (webUri != null) {
-        outContent.webUri = Uri.parse(webUri)
-      } else {
-        outContent.webUri = null
-      }
-    }
   }
 }

a23037ce704d8deee87a10e6a41dbd7f0a04136b
Harsh Shandilya <me@msfjarvis.dev>
2024-01-11 01:54:07 +0530

    fix(build): add missing plugin

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022-2023 Harsh Shandilya.
+ * Copyright © 2022-2024 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
@@ -7,6 +7,8 @@
 @file:Suppress("UnstableApiUsage")
 
 rootProject.name = "build-logic"
+
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0" }
 
 dependencyResolutionManagement {
   repositories {

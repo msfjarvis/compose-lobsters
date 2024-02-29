fdf891c8eb4f4f2187a2d339f97b9549141ba5c9
Harsh Shandilya <me@msfjarvis.dev>
2024-02-29 14:15:01 +0530

    fix(build): disable ObsoleteLintCustomCheck lint
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2023 Harsh Shandilya.
+ * Copyright © 2023-2024 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
@@ -16,6 +16,8 @@ android {
   buildFeatures.compose = true
   composeOptions.kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
   kotlin.explicitApi()
+  // Don't quite care
+  lint.disable += "DeprecatedCall"
 }
 
 dependencies {

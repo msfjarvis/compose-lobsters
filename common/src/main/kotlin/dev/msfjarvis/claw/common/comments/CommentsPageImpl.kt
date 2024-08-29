bf5296677257447c73bb0f786f27cdeedd3db0d3
Harsh Shandilya <me@msfjarvis.dev>
2024-08-29 16:34:43 +0530

    fix(common): re-add spacer to comments page

--- common/src/main/kotlin/dev/msfjarvis/claw/common/comments/CommentsPageImpl.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/comments/CommentsPageImpl.kt
@@ -14,7 +14,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
@@ -91,6 +93,8 @@ internal fun CommentsPageInternal(
         commentNodes.forEach { node ->
           item(key = node.comment.shortId) { Node(node, htmlConverter, openUserProfile) }
         }
+
+        item(key = "bottom_spacer") { Spacer(modifier = Modifier.height(16.dp)) }
       } else {
         item {
           Text(

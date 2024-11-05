b24a180780d6999f7758c015722f4449420c1cca
Harsh Shandilya <me@msfjarvis.dev>
2024-11-05 13:09:47 +0530

    refactor: migrate to Coil3

--- common/src/main/kotlin/dev/msfjarvis/claw/common/injection/CoilAppPlugin.kt
+++ common/src/main/kotlin/dev/msfjarvis/claw/common/injection/CoilAppPlugin.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2022 Harsh Shandilya.
+ * Copyright © 2022-2024 Harsh Shandilya.
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
@@ -7,10 +7,12 @@
 package dev.msfjarvis.claw.common.injection
 
 import android.app.Application
-import coil.Coil
-import coil.ImageLoader
-import coil.disk.DiskCache
-import coil.memory.MemoryCache
+import coil3.ImageLoader
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.request.crossfade
 import com.deliveryhero.whetstone.app.ApplicationScope
 import com.squareup.anvil.annotations.ContributesMultibinding
 import dev.msfjarvis.claw.core.injection.AppPlugin
@@ -19,9 +21,11 @@ import javax.inject.Inject
 @ContributesMultibinding(ApplicationScope::class)
 class CoilAppPlugin @Inject constructor() : AppPlugin {
   override fun apply(application: Application) {
-    Coil.setImageLoader {
+    SingletonImageLoader.setSafe {
       ImageLoader.Builder(application)
-        .memoryCache { MemoryCache.Builder(application).maxSizePercent(MEMORY_CACHE_RATIO).build() }
+        .memoryCache {
+          MemoryCache.Builder().maxSizePercent(application, MEMORY_CACHE_RATIO).build()
+        }
         .diskCache {
           DiskCache.Builder()
             .directory(application.cacheDir.resolve("image_cache"))

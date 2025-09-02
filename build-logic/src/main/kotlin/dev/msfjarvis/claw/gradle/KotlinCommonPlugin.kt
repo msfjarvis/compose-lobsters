e1c24783c4fbf615956d4db51152394fa0eec7d9
Harsh Shandilya <me@msfjarvis.dev>
2025-09-02 23:19:11 +0530

    fix(build): switch out of JVM toolchains

--- build-logic/src/main/kotlin/dev/msfjarvis/claw/gradle/KotlinCommonPlugin.kt
+++ build-logic/src/main/kotlin/dev/msfjarvis/claw/gradle/KotlinCommonPlugin.kt
@@ -6,13 +6,10 @@
  */
 package dev.msfjarvis.claw.gradle
 
-import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.gradle.jvm.toolchain.JavaToolchainSpec
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -53,8 +50,5 @@ class KotlinCommonPlugin : Plugin<Project> {
         "-Xjspecify-annotations=strict",
         "-Xtype-enhancement-improvements-strict-mode",
       )
-
-    val JVM_TOOLCHAIN_ACTION =
-      Action<JavaToolchainSpec> { languageVersion.set(JavaLanguageVersion.of(17)) }
   }
 }

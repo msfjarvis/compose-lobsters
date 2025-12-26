a725db048395037587afb962943a7b2cfe0f8f13
Harsh Shandilya <me@msfjarvis.dev>
2025-12-26 21:22:40 +0530

    fix: exclude non-lobsters domains from Sentry instrumentation
    Fixes COMPOSE-LOBSTERS-7M

---
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/injection/SentryConfigurationPlugin.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright © Harsh Shandilya.
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package dev.msfjarvis.claw.android.injection
+
+import android.app.Application
+import dev.msfjarvis.claw.api.LobstersApi
+import dev.msfjarvis.claw.core.injection.AppPlugin
+import dev.zacsweers.metro.AppScope
+import dev.zacsweers.metro.ContributesIntoSet
+import dev.zacsweers.metro.Inject
+import io.sentry.SentryOptions
+import io.sentry.TypeCheckHint.OKHTTP_REQUEST
+import io.sentry.android.core.SentryAndroid
+import okhttp3.Request
+
+@ContributesIntoSet(AppScope::class)
+@Inject
+class SentryConfigurationPlugin : AppPlugin {
+  override fun apply(application: Application) {
+    SentryAndroid.init(application) { options ->
+      options.beforeSend =
+        SentryOptions.BeforeSendCallback { event, hint ->
+          val request = hint.getAs(OKHTTP_REQUEST, Request::class.java)
+
+          // Drop all OkHttp errors that are not about Lobsters specifically.
+          if (request != null && LobstersApi.BASE_URL.contains(request.url.host)) {
+            null
+          } else {
+            event
+          }
+        }
+    }
+  }
+}

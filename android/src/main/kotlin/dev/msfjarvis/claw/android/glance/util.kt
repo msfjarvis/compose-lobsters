3390031120f1209e8a2ef7a3eaff9e92c8baafbd
Harsh Shandilya <me@msfjarvis.dev>
2026-01-01 02:51:52 +0530

    refactor(glance): clean up and consolidate

--- android/src/main/kotlin/dev/msfjarvis/claw/android/glance/util.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/glance/util.kt
@@ -6,8 +6,30 @@
  */
 package dev.msfjarvis.claw.android.glance
 
+import android.annotation.SuppressLint
+import android.os.Build
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.glance.GlanceComposable
+import androidx.glance.GlanceTheme
+import androidx.glance.material3.ColorProviders
+import dev.msfjarvis.claw.common.posts.TEST_POST
+import dev.msfjarvis.claw.common.theme.DarkThemeColors
+import dev.msfjarvis.claw.common.theme.LightThemeColors
+import kotlinx.collections.immutable.toPersistentList
+
+@Composable
+fun LobstersGlanceTheme(content: @GlanceComposable @Composable () -> Unit) {
+  GlanceTheme(
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) GlanceTheme.colors
+    else ColorProviders(light = LightThemeColors, dark = DarkThemeColors),
+    content,
+  )
+}
+
+@SuppressLint("VisibleForTests")
+fun samplePosts() = buildList(5) { repeat(5) { add(TEST_POST) } }.toPersistentList()
 
 fun FontWeight?.toGlance(): androidx.glance.text.FontWeight? {
   return when (this) {

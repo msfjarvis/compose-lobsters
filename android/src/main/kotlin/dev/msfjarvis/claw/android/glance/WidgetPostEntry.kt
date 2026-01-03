4c0089676aa2f1525f88a92b66ed8bb8825fb579
Harsh Shandilya <me@msfjarvis.dev>
2026-01-03 20:18:49 +0530

    fix(android): use `actionStartActivity` everywhere in widgets
    Fixes COMPOSE-LOBSTERS-9P

--- android/src/main/kotlin/dev/msfjarvis/claw/android/glance/WidgetPostEntry.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/glance/WidgetPostEntry.kt
@@ -6,17 +6,19 @@
  */
 package dev.msfjarvis.claw.android.glance
 
+import android.content.Intent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.glance.GlanceComposable
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.Image
 import androidx.glance.ImageProvider
-import androidx.glance.action.actionParametersOf
+import androidx.glance.LocalContext
 import androidx.glance.action.clickable
-import androidx.glance.appwidget.action.actionRunCallback
+import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Row
@@ -24,6 +26,7 @@ import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
+import dev.msfjarvis.claw.android.MainActivity
 import dev.msfjarvis.claw.android.R
 import dev.msfjarvis.claw.model.UIPost
 
@@ -32,12 +35,15 @@ import dev.msfjarvis.claw.model.UIPost
 fun WidgetPostEntry(post: UIPost, modifier: GlanceModifier = GlanceModifier) {
   val titleStyle = MaterialTheme.typography.titleMedium
   val commentsAction =
-    actionRunCallback<OpenCommentsCallback>(
-      actionParametersOf(OpenCommentsCallback.postIdKey to post.shortId)
+    actionStartActivity(
+      Intent(Intent.ACTION_VIEW, "claw://comments/${post.shortId}".toUri()).apply {
+        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        setClass(LocalContext.current, MainActivity::class.java)
+      }
     )
   val postAction =
     if (post.url.startsWith('/') || post.url.isEmpty()) commentsAction
-    else actionRunCallback<OpenUrlCallback>(actionParametersOf(OpenUrlCallback.urlKey to post.url))
+    else actionStartActivity(Intent(Intent.ACTION_VIEW, post.url.toUri()))
   Row(
     verticalAlignment = Alignment.CenterVertically,
     modifier =

14b5a4877f9412db1c2f59854cae29e0e5f1d5d1
Harsh Shandilya <me@msfjarvis.dev>
2026-07-30 10:04:11 +0530

    fix: make settings screen scrollable

--- android/src/main/kotlin/dev/msfjarvis/claw/android/ui/screens/SettingsScreen.kt
+++ android/src/main/kotlin/dev/msfjarvis/claw/android/ui/screens/SettingsScreen.kt
@@ -17,6 +17,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.LibraryBooks
 import androidx.compose.material.icons.filled.AccountCircle
@@ -83,7 +85,7 @@ fun SettingsScreen(
   modifier: Modifier = Modifier,
 ) {
   val coroutineScope = rememberCoroutineScope()
-  Column(modifier.padding(contentPadding)) {
+  Column(modifier.padding(contentPadding).verticalScroll(rememberScrollState())) {
     // Account Section
     SectionHeader(title = "Account")
     if (isLoggedIn) {
